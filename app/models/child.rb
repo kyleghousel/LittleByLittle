9c6ac7de54660688dc208b02631ee286b00a890b
Kyle <kylegranthousel@gmail.com>
2025-08-06 15:23:57 -0400

    Add model validation

--- app/models/child.rb
+++ app/models/child.rb
@@ -1,4 +1,7 @@
 class Child < ActiveRecord::Base
-  has_many :entries
+  has_many :entries, dependent: :destroy
   has_many :milestones, through: :entries
+
+  validates :name, presence: true
+  validates :birthdate, presence: true
 end
--- app/models/milestone.rb
+++ app/models/milestone.rb
@@ -1,4 +1,7 @@
 class Milestone < ActiveRecord::Base
-  has_many :entries
+  has_many :entries, dependent: :destroy
   has_many :children, through: :entries
+
+  validates :title, presence: true, uniqueness: true
+  validates :milestone_type, presence: true
 end
